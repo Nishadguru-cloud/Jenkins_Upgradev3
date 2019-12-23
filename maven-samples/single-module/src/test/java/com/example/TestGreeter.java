7e26c3046ca00f717275a564d5ed2e02a6fd6ea2
Nishad <nishadguru.s@gmail.com>
2019-12-23 12:35:03 +0530

    Changes3

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -46,7 +46,7 @@ public class TestGreeter {
   }
 
    @Test
-  public void greetShouldIncludeGreetingMessage() {
+  public void greetShouldIncludeGreetingMessage2() {
     String someone = "World, Thanks you making me Happy But I'm really not at all Happy";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
